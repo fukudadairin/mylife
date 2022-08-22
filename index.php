8033e2bda03fa3b72f4a7b129743539e12c864c5
fukudadairin <79754701+fukudadairin@users.noreply.github.com>
2022-08-23 08:46:49 +0900

    [収支一覧]各出費項目毎の目標金額を入力できる欄を設置。#6

--- index.php
+++ index.php
@@ -24,87 +24,94 @@
 <body>
     <h1>my money</h1>
     <div class="p30">
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th scope="col">総合計</th>
-                    <th scope="col">変動費</th>
-                    <th scope="col">固定費</th>
-                    <th scope="col">食費</th>
-                    <th scope="col">旅費交通費</th>
-                    <th scope="col">水道ガスなど</th>
-                    <th scope="col">消耗品</th>
-                    <th scope="col">通信費</th>
-                    <th scope="col">研修費</th>
-                    <th scope="col">接待交際費</th>
-                    <th scope="col">医療費</th>
-                    <th scope="col">節税関係</th>
-                    <th scope="col">返済</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">合計</th>
-                    <td>3,000</td>
-                    <td>3,000</td>
-                    <td>3,000</td>
-                    <td>3,000</td>
-                    <td>3,000</td>
-                    <td>3,000</td>
-                    <td>3,000</td>
-                    <td>3,000</td>
-                    <td>3,000</td>
-                    <td>3,000</td>
-                    <td>3,000</td>
-                    <td>3,000</td>
-                </tr>
-                <tr>
-                    <th scope="row">平均</th>
-                    <td>1,000</td>
-                    <td>4,000</td>
-                    <td>6,000</td>
-                    <td>1,000</td>
-                    <td>4,000</td>
-                    <td>6,000</td>
-                    <td>1,000</td>
-                    <td>4,000</td>
-                    <td>6,000</td>
-                    <td>1,000</td>
-                    <td>4,000</td>
-                    <td>6,000</td>
-                </tr>
-                <tr>
-                    <th scope="row">目標</th>
-                    <td>4,000</td>
-                    <td>3,000</td>
-                    <td>4,000</td>
-                    <td>3,000</td>
-                    <td>4,000</td>
-                    <td>3,000</td>
-                    <td>4,000</td>
-                    <td>3,000</td>
-                    <td>4,000</td>
-                    <td>4,000</td>
-                    <td>4,000</td>
-                    <td>4,000</td>
-                </tr>
-                <tr>
-                    <th scope="row">先月比</th>
-                    <td>−30,4％</td>
-                    <td>−30,4％</td>
-                    <td>−10,4％</td>
-                    <td>−30,4％</td>
-                    <td>−10,4％</td>
-                    <td>−30,4％</td>
-                    <td>−10,4％</td>
-                    <td>−30,4％</td>
-                    <td>−10,4％</td>
-                    <td>−10,4％</td>
-                    <td>−10,4％</td>
-                    <td>−10,4％</td>
-                </tr>
-            </tbody>
-        </table>
+        <form method="POST">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th scope="col">総合計</th>
+                        <th scope="col">変動費</th>
+                        <th scope="col">固定費</th>
+                        <th scope="col">食費</th>
+                        <th scope="col">旅費交通費</th>
+                        <th scope="col">水道ガスなど</th>
+                        <th scope="col">消耗品</th>
+                        <th scope="col">通信費</th>
+                        <th scope="col">研修費</th>
+                        <th scope="col">接待交際費</th>
+                        <th scope="col">医療費</th>
+                        <th scope="col">節税関係</th>
+                        <th scope="col">返済</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">合計</th>
+                        <td>3,000</td>
+                        <td>3,000</td>
+                        <td>3,000</td>
+                        <td>3,000</td>
+                        <td>3,000</td>
+                        <td>3,000</td>
+                        <td>3,000</td>
+                        <td>3,000</td>
+                        <td>3,000</td>
+                        <td>3,000</td>
+                        <td>3,000</td>
+                        <td>3,000</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">平均</th>
+                        <td>1,000</td>
+                        <td>4,000</td>
+                        <td>6,000</td>
+                        <td>1,000</td>
+                        <td>4,000</td>
+                        <td>6,000</td>
+                        <td>1,000</td>
+                        <td>4,000</td>
+                        <td>6,000</td>
+                        <td>1,000</td>
+                        <td>4,000</td>
+                        <td>6,000</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">目標</th>
+                        <td><input class="w100" type="text"> </td>
+                        <td><input class="w100" type="text"></td>
+                        <td><input class="w100" type="text"></td>
+                        <td><input class="w100" type="text"></td>
+                        <td><input class="w100" type="text"></td>
+                        <td><input class="w100" type="text"></td>
+                        <td><input class="w100" type="text"></td>
+                        <td><input class="w100" type="text"></td>
+                        <td><input class="w100" type="text"></td>
+                        <td><input class="w100" type="text"></td>
+                        <td><input class="w100" type="text"></td>
+                        <td><input class="w100" type="text"></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">先月比</th>
+                        <td>−30,4％</td>
+                        <td>−30,4％</td>
+                        <td>−10,4％</td>
+                        <td>−30,4％</td>
+                        <td>−10,4％</td>
+                        <td>−30,4％</td>
+                        <td>−10,4％</td>
+                        <td>−30,4％</td>
+                        <td>−10,4％</td>
+                        <td>−10,4％</td>
+                        <td>−10,4％</td>
+                        <td>−10,4％</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <button class="btn btn-primary rounded-pill btnLayout" type="submit">更新</button>
+
+        </form>
+
+
     </div>
 
 
@@ -162,7 +169,7 @@
         <div class="p30 w50">
             <div class="t_content_title">現金：104400円</div>
             <table class="table table-bordered">
-            <thead>
+                <thead>
                     <tr>
                         <th scope="col">ID</th>
                         <th scope="col">出費項目</th>
